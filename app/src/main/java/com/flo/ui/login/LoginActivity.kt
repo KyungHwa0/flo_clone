ca4e6f86afff4a3a6e2db33ce7c5bee7e58a8cce
kyunghwa <snoopn303@gmail.com>
2024-01-01 22:31:07 +0900

    초반 설계

---
+++ app/src/main/java/com/flo/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.flo.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
