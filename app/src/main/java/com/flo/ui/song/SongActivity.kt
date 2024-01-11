b6b4f47b56761f899614401c080493094c69da0f
kyunghwa <snoopn303@gmail.com>
2024-01-11 13:36:03 +0900

    SongActivity 추가

---
+++ app/src/main/java/com/flo/ui/song/SongActivity.kt
@@ -0,0 +1,12 @@
+package com.flo.ui.song
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flo.R
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+    }
+}
