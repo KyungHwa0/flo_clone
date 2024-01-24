47bfe5e5ce33d7b04dbc8283c1d5aee888ec0e43
kyunghwa <snoopn303@gmail.com>
2024-01-25 01:39:27 +0900

    메인 엑티비티 / 프래그먼트 뷰바인딩

--- app/src/main/java/com/flo/ui/main/MainActivity.kt
+++ app/src/main/java/com/flo/ui/main/MainActivity.kt
@@ -3,10 +3,16 @@ package com.flo.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flo.R
+import com.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
