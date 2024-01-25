d342ef9ae05fee280a98a66f3e32c33c705a49bd
kyunghwa <snoopn303@gmail.com>
2024-01-25 23:51:56 +0900

    정리

--- app/src/main/java/com/flo/ui/main/home/BannerFragment.kt
+++ app/src/main/java/com/flo/ui/main/home/BannerFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.flo.databinding.FragmentBannerBinding
-import com.flo.databinding.FragmentHomeBinding
 
 class BannerFragment(val imgRes : Int) : Fragment() {
     private lateinit var binding : FragmentBannerBinding
