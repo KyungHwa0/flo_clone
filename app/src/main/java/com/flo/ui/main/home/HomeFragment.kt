500ba7b7810d4c367da51723abc7572ec3015767
kyunghwa <snoopn303@gmail.com>
2024-01-25 23:42:35 +0900

    Banner 기능 추가

--- app/src/main/java/com/flo/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/flo/ui/main/home/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.flo.R
 import com.flo.databinding.FragmentHomeBinding
+import com.flo.ui.main.MainActivity
+import com.flo.ui.main.album.AlbumFragment
 
 class HomeFragment : Fragment() {
     private lateinit var binding : FragmentHomeBinding
@@ -15,6 +19,17 @@ class HomeFragment : Fragment() {
         container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.homePannelAlbumImg01Iv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
+        }
+
+        val bannerAdapter = BannerViewPagerAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_01))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_02))
+        binding.homeBannerVp.adapter = bannerAdapter
+        // 좌우 스크롤
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         return binding.root
     }
 }
