5b6a4c60822892c5aa9d03af795fb271b8dae1c0
kyunghwa <snoopn303@gmail.com>
2024-02-02 15:00:18 +0900

    쓰레드를 이용한 seekbar / exoplayer을 이용한 노래재생

--- app/src/main/java/com/flo/ui/song/SongActivity.kt
+++ app/src/main/java/com/flo/ui/song/SongActivity.kt
@@ -1,38 +1,144 @@
 package com.flo.ui.song
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.flo.R
+import com.flo.data.Song
 import com.flo.databinding.ActivitySongBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 
 class SongActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySongBinding
+    private lateinit var song: Song
+    private lateinit var timer: Timer
+    private var exoPlayer: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        binding.songDownIb.setOnClickListener { 
+
+        initSong()
+        setPlayer(song)
+
+        binding.songDownIb.setOnClickListener {
             finish()
-        }
-        
-        binding.songMiniplayerIv.setOnClickListener { 
-            setPlayerStatus(false)
         }
 
         binding.songMiniplayerIv.setOnClickListener {
             setPlayerStatus(true)
         }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+
     }
 
-    fun setPlayerStatus(isPlaying: Boolean) {
+    override fun onDestroy() {
+        super.onDestroy()
+        // ExoPlayer 리소스 해제
+        exoPlayer?.release()
+        exoPlayer = null
+        // 타이머 쓰레드 중지
+        timer.interrupt()
+    }
+
+    private fun initSong() {
+        if (intent.hasExtra("title") && intent.hasExtra("singer")) {
+            song = Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second", 0),
+                intent.getIntExtra("playTime", 0),
+                intent.getBooleanExtra("isPlaying", false),
+                intent.getStringExtra("music")!!
+            )
+        }
+        startTimer()
+    }
+
+    private fun setPlayer(songEntity: Song) {
+        binding.songMusicTitleTv.text = intent.getStringExtra("title")!!
+        binding.songSingerNameTv.text = intent.getStringExtra("singer")!!
+        binding.songStartTimeTv.text =
+            String.format("%02d:%02d", songEntity.second / 60, songEntity.second % 60)
+        binding.songEndTimeTv.text =
+            String.format("%02d:%02d", songEntity.playTime / 60, songEntity.playTime % 60)
+        binding.songProgressSb.progress = (songEntity.second * 1000 / songEntity.playTime)
+        exoPlayer = SimpleExoPlayer.Builder(this).build()
+        val musicResourceId = resources.getIdentifier(songEntity.music, "raw", packageName)
+        val mediaItem = MediaItem.fromUri(Uri.parse("android.resource://$packageName/$musicResourceId"))
+        exoPlayer?.setMediaItem(mediaItem)
+        exoPlayer?.prepare()
+        binding.playerView.player = exoPlayer
+        setPlayerStatus(songEntity.isPlaying)
+    }
+
+
+
+    private fun setPlayerStatus(isPlaying: Boolean) {
+        song.isPlaying = isPlaying
+        timer.isPlaying = isPlaying
+
         if (isPlaying) {
-            binding.songMiniplayerIv.visibility = View.VISIBLE
-            binding.songPauseIv.visibility = View.GONE
-        } else {
             binding.songMiniplayerIv.visibility = View.GONE
             binding.songPauseIv.visibility = View.VISIBLE
+            exoPlayer?.play()
+        } else {
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+            if(exoPlayer?.isPlaying == true) {
+                exoPlayer?.pause()
+            }
+        }
+    }
+
+
+    private fun startTimer() {
+        timer = Timer(song.playTime, song.isPlaying)
+        timer.start()
+    }
+
+    inner class Timer(private val playTime: Int, var isPlaying: Boolean = true) : Thread() {
+        private var second: Int = 0
+        private var mills: Float = 0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true) {
+
+                    if (second >= playTime) {
+                        break
+                    }
+
+                    if (isPlaying) {
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread {
+                            binding.songProgressSb.progress = ((mills / playTime) * 100).toInt()
+                        }
+
+                        if (mills % 1000 == 0f) {
+                            runOnUiThread {
+                                binding.songStartTimeTv.text =
+                                    String.format("%02d:%02d", second / 60, second % 60)
+                            }
+                            second++
+                        }
+
+                    }
+                }
+            } catch (e: InterruptedException) {
+                Log.d("Song", "쓰레드가 죽었습니다. ${e.message}")
+            }
+
         }
     }
 }
