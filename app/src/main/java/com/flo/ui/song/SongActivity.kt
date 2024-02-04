0ca09ce7caf36c44ad9b656302b72972a89864d8
kyunghwa <snoopn303@gmail.com>
2024-02-04 17:09:12 +0900

    더미데이터 추가(ROOM)한 데이터로 노래 재생 구현

--- app/src/main/java/com/flo/ui/song/SongActivity.kt
+++ app/src/main/java/com/flo/ui/song/SongActivity.kt
@@ -5,27 +5,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.flo.data.Song
+import com.flo.data.SongDatabase
 import com.flo.databinding.ActivitySongBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.gson.Gson
 
 class SongActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySongBinding
-    private lateinit var song: Song
     private lateinit var timer: Timer
     private var exoPlayer: ExoPlayer? = null
+    private var gson: Gson = Gson()
+
+    val songs = arrayListOf<Song>()
+    lateinit var songDB: SongDatabase
+    var nowPos = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initPlayList()
         initSong()
-        setPlayer(song)
+        initClickListener()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        // 타이머 쓰레드 중지
+        timer.interrupt()
+        // ExoPlayer가 갖고 있던 리소스 해제
+        exoPlayer?.release()
+        // ExoPlayer가 해제
+        exoPlayer = null
+    }
+
+    private fun initPlayList(){
+        songDB = SongDatabase.getInstance(this)!!
+        songs.addAll(songDB.songDao().getSongs())
+    }
+
+    private fun initClickListener(){
         binding.songDownIb.setOnClickListener {
             finish()
         }
@@ -37,52 +62,82 @@ class SongActivity : AppCompatActivity() {
             setPlayerStatus(false)
         }
 
-    }
+        binding.songNextIv.setOnClickListener {
+            moveSong(+1)
+        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        // ExoPlayer 리소스 해제
-        exoPlayer?.release()
-        exoPlayer = null
-        // 타이머 쓰레드 중지
-        timer.interrupt()
+        binding.songPreviousIv.setOnClickListener {
+            moveSong(-1)
+        }
     }
 
     private fun initSong() {
-        if (intent.hasExtra("title") && intent.hasExtra("singer")) {
-            song = Song(
-                intent.getStringExtra("title")!!,
-                intent.getStringExtra("singer")!!,
-                intent.getIntExtra("second", 0),
-                intent.getIntExtra("playTime", 0),
-                intent.getBooleanExtra("isPlaying", false),
-                intent.getStringExtra("music")!!
-            )
-        }
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val songId = sharedPreferences.getInt("songId",0)
+
+        nowPos = getPlayingSongPosition(songId)
+
+        Log.d("now Song ID",songs[nowPos].id.toString())
+
         startTimer()
+        setPlayer(songs[nowPos])
     }
 
-    private fun setPlayer(songEntity: Song) {
-        binding.songMusicTitleTv.text = intent.getStringExtra("title")!!
-        binding.songSingerNameTv.text = intent.getStringExtra("singer")!!
+    private fun moveSong(direct: Int){
+        if (nowPos + direct < 0){
+            Toast.makeText(this,"first song",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (nowPos + direct >= songs.size){
+            Toast.makeText(this,"last song",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        nowPos += direct
+
+        // 타이머가 작동 중이라면 중지합니다.
+        if (this::timer.isInitialized && timer.isAlive) {
+            timer.interrupt()
+        }
+
+        exoPlayer?.stop()
+        exoPlayer?.release()
+        exoPlayer = null
+
+        // 새로운 노래를 설정합니다.
+        setPlayer(songs[nowPos])
+    }
+
+    private fun getPlayingSongPosition(songId: Int): Int{
+        for (i in 0 until songs.size){
+            if (songs[i].id == songId){
+                return i
+            }
+        }
+        return 0
+    }
+
+    private fun setPlayer(song: Song) {
+        binding.songMusicTitleTv.text = song.title
+        binding.songSingerNameTv.text = song.singer
         binding.songStartTimeTv.text =
-            String.format("%02d:%02d", songEntity.second / 60, songEntity.second % 60)
+            String.format("%02d:%02d", song.second / 60, song.second % 60)
         binding.songEndTimeTv.text =
-            String.format("%02d:%02d", songEntity.playTime / 60, songEntity.playTime % 60)
-        binding.songProgressSb.progress = (songEntity.second * 1000 / songEntity.playTime)
+            String.format("%02d:%02d", song.playTime / 60, song.playTime % 60)
+        binding.songAlbumIv.setImageResource(song.coverImg!!)
+        binding.songProgressSb.progress = (song.second * 1000 / song.playTime)
         exoPlayer = SimpleExoPlayer.Builder(this).build()
-        val musicResourceId = resources.getIdentifier(songEntity.music, "raw", packageName)
+        val musicResourceId = resources.getIdentifier(song.music, "raw", packageName)
         val mediaItem = MediaItem.fromUri(Uri.parse("android.resource://$packageName/$musicResourceId"))
         exoPlayer?.setMediaItem(mediaItem)
         exoPlayer?.prepare()
         binding.playerView.player = exoPlayer
-        setPlayerStatus(songEntity.isPlaying)
+        setPlayerStatus(song.isPlaying)
     }
 
-
-
     private fun setPlayerStatus(isPlaying: Boolean) {
-        song.isPlaying = isPlaying
+        songs[nowPos].isPlaying = isPlaying
         timer.isPlaying = isPlaying
 
         if (isPlaying) {
@@ -98,9 +153,8 @@ class SongActivity : AppCompatActivity() {
         }
     }
 
-
     private fun startTimer() {
-        timer = Timer(song.playTime, song.isPlaying)
+        timer = Timer(songs[nowPos].playTime, songs[nowPos].isPlaying)
         timer.start()
     }
 
